847d53af86a0c0ed6831551edf83a43f449da7ea
Jason Ho <raktos@uw.edu>
2015-10-20 16:56:27 -0700

    should be finished

--- domain-modeling/domain-modeling/main.swift
+++ domain-modeling/domain-modeling/main.swift
@@ -8,6 +8,15 @@
 
 import Foundation
 
+protocol CustomStringConvertable {
+    var description : String { get }
+}
+
+protocol Mathematics {
+    mutating func add(amt : Double, currency : Currency)
+    mutating func subtract(amt : Double, currency : Currency)
+}
+
 //currency enum
 enum Currency {
         case usd, gbp, eur, can
@@ -15,15 +24,26 @@ enum Currency {
 
 //Money struct
 //functions more like a bank account, the spec was a bit vague this is my best interpretation
-struct Money {
+struct Money : CustomStringConvertable, Mathematics {
     //actual value ALWAYS STORED IN USD
-    var amount : Double
+    var amount = 0.0
     //currency type
-    var currency : Currency
+    var currency = Currency.usd
     
-    init(amount : Double, currency : Currency) {
-        self.amount = amount
-        self.currency = currency
+    //description from protoocol
+    var description : String {
+        switch self.currency {
+        case .can: return "CAN\(getAmount())"
+        case .eur: return "EUR\(getAmount())"
+        case .gbp: return "GBP\(getAmount())"
+        default: return "USD\(getAmount())"
+        }
+    }
+    
+    init(var incomingAmount : Double, incomingCurrency : Currency) {
+        incomingAmount = convertToUSD(incomingAmount, currency: incomingCurrency)
+        self.amount = incomingAmount
+        self.currency = incomingCurrency
     }
     
     //convert changes the "type" of currency, the amount is independant
@@ -54,7 +74,7 @@ struct Money {
     }
     
     //converts to USD, only used internaly for mathematics
-    private func convertToUSD(var amt : Double, currency : Currency) -> Double {
+    private func convertToUSD(amt : Double, currency : Currency) -> Double {
         switch currency {
         case .can: return amt * 0.8
         case .eur: return amt * (2.0 / 3.0)
@@ -62,25 +82,46 @@ struct Money {
         default : return amt
         }
     }
+    
+    //Mathematics protocol
 }
 
 print("Money Tests:")
-var m = Money(amount : 0.0, currency : Currency.usd)
+var m = Money(incomingAmount : 0.0, incomingCurrency : Currency.usd)
 print("Created a Money in USD with 0.0\n")
+print("CustomStringConvertable test: " + m.description + "\n")
+
 m.add(5.0, currency : Currency.gbp)
 print("added 5GBP. currently contains \(m.getAmount()) in USD\n")
+print("CustomStringConvertable test: " + m.description + "\n")
+
 m.add(10, currency : Currency.can)
 print("added 10CAN. currently contains \(m.getAmount()) in USD\n")
+print("CustomStringConvertable test: " + m.description + "\n")
+
 m.add(9, currency : Currency.eur)
 print("added 9EUR. currently contains\(m.getAmount()) in USD\n")
+print("CustomStringConvertable test: " + m.description + "\n")
+
 m.convert(Currency.gbp)
 print("convert to GBP. currently contains \(m.getAmount()) in GBP \n")
+print("CustomStringConvertable test: " + m.description + "\n")
+
 m.convert(Currency.eur)
 print("convert to EUR. currently contains \(m.getAmount()) in EUR\n")
+print("CustomStringConvertable test: " + m.description + "\n")
+
 m.convert(Currency.can)
 print("conver to CAN. currently contains \(m.getAmount()) in CAN\n")
+print("CustomStringConvertable test: " + m.description + "\n")
+
 m.add(10.0, currency : Currency.gbp)
 print("added 10GBP. currently contains \(m.getAmount()) in CAN \n")
+print("CustomStringConvertable test: " + m.description + "\n")
+
+print("\nMathematics protocol tests: \n")
+
+
 print("\n")
 
 //salary type
@@ -89,10 +130,19 @@ enum SalaryType {
 }
 
 //Job class
-class Job {
+class Job : CustomStringConvertable {
     var title : String
     var salary : Double
     var salaryType : SalaryType
+    
+    var description : String {
+        var type = ""
+        switch self.salaryType{
+        case .hourly: type = "Hourly"
+        case .yearly: type = "Yearly"
+        }
+        return "Job Title: \(self.title), Salary: \(self.salary), Salary Type: \(type)"
+    }
     
     init(title : String, salary : Double, salaryType : SalaryType) {
         self.title = title
@@ -117,19 +167,30 @@ class Job {
 print("Job Tests:\n")
 var j = Job(title : "programmer", salary : 80000.0, salaryType : SalaryType.yearly)
 print("Job with income of \(j.calculateIncome()) yearly\n")
+print("CustomStringConvertable test: " + j.description + "\n")
+
 j.raise(10)
 print("raise of 10%. income is now \(j.calculateIncome())\n")
+print("CustomStringConvertable test: " + j.description + "\n")
+
 j = Job(title : "lab tech", salary : 20.0, salaryType : SalaryType.hourly)
 print("Job with income of 20 hourly makes \(j.calculateIncome(hours: 100.0)) in 100 hours\n")
+print("CustomStringConvertable test: " + j.description + "\n")
 print("\n")
 
 //Person class
-class Person {
+class Person : CustomStringConvertable {
     let firstName : String
     let lastName : String
     var age : Int
     var job : Job?
     weak var spouse : Person?
+    
+    var description : String {
+        let title = self.job != nil ? self.job!.description : "unemployed"
+        let spouseName = self.spouse != nil ? "\(self.spouse!.firstName) \(self.spouse!.lastName)" : "unmarried"
+        return "\(self.firstName) \(self.lastName)\nAge: \(self.age) years\nJob: \(title)\nSpouse: \(spouseName)\n"
+    }
     
     init(firstName : String, lastName : String, age : Int, job : Job? = nil, spouse : Person? = nil) {
         self.firstName = firstName
@@ -162,16 +223,31 @@ class Person {
 print("Person Tests:\n")
 var p1 = Person(firstName : "John", lastName : "Dole", age : 47, job : j)
 print("\(p1.toString())\n")
+print("CustomStringConvertable test: \n" + p1.description + "\n")
+
 var p2 = Person(firstName : "Joanne", lastName : "Dole", age : 45, spouse : p1)
 print("\(p2.toString())\n")
+print("CustomStringConvertable test: \n" + p2.description + "\n")
+
 p1.spouse = p2
 print("\(p1.toString())\n")
+print("CustomStringConvertable test: \n" + p1.description + "\n")
+
 var p3 = Person(firstName : "Bobby", lastName : "Dole", age : 15, job : j, spouse : p1)
 print("\(p3.toString())\n")
+print("CustomStringConvertable test: \n" + p3.description + "\n")
 
 //family class
-class Family {
+class Family : CustomStringConvertable {
     var members : [Person] = []
+    
+    var description : String {
+        var s = ""
+        for p in self.members {
+            s += "\(p.description)\n"
+        }
+        return s
+    }
     
     //initialisation will fail and return nil if there are no passed member 21 or older
     init?(members : [Person]) {
@@ -214,6 +290,7 @@ if f == nil {
         print("\(p.toString())\n")
     }
 }
+print("CustomStringConvertable test: \n" + f!.description + "\n")
 
 print("Added new faily member Mary\n")
 f!.haveChild("Mary", lastName : "Dole")
@@ -224,6 +301,7 @@ if f == nil {
         print("\(p.toString())\n")
     }
 }
+print("CustomStringConvertable test: \n" + f!.description + "\n")
 
 f = Family(members: [p3])
 print("Creating family of just Bob\n")
@@ -234,3 +312,18 @@ if f == nil {
         print("\(p.toString())\n")
     }
 }
+
+//Double Extension
+extension Double {
+    var USD : Money { return Money(incomingAmount: self, incomingCurrency: Currency.usd) }
+    var EUR : Money { return Money(incomingAmount: self, incomingCurrency: Currency.eur) }
+    var GBP : Money { return Money(incomingAmount: self, incomingCurrency: Currency.gbp) }
+    var CAN : Money { return Money(incomingAmount: self, incomingCurrency: Currency.can) }
+}
+
+//Double extension tests
+print("\n\nDouble extension tests:\n")
+print(10.0.USD.description + "\n")
+print(10.0.EUR.description + "\n")
+print(10.0.GBP.description + "\n")
+print(10.0.CAN.description + "\n")
